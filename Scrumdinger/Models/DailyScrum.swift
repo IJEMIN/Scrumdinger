ba0dd28948a6407026cc9da5125cf5106e082f05
i_jemin <i_jemin@icloud.com>
2022-05-28 14:24:33 +0900

    DailyScrum - Add instantiate method using DailyScrumData type

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -47,6 +47,15 @@ extension DailyScrum {
         lengthInMinutes = Int(data.lengthInMinutes)
         theme = data.theme
     }
+    
+    init(data: Data)
+    {
+        id = UUID()
+        title = data.title
+        attendees = data.attendees
+        lengthInMinutes = Int(data.lengthInMinutes)
+        theme = data.theme
+    }
 }
 
 extension DailyScrum {
