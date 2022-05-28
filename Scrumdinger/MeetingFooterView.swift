a70016921e650857f3d75eb78312b6d9427878b8
i_jemin <i_jemin@icloud.com>
2022-05-28 12:09:43 +0900

    Add MeetingFooterView

---
+++ Scrumdinger/MeetingFooterView.swift
@@ -0,0 +1,28 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by Jemin Lee on 5/28/22.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        HStack {
+            Text("Speaker 1 of 3")
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: "forward.fill")
+            }
+            .accessibilityLabel("Next speaker")
+        }
+    }
+}
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingFooterView()
+            .previewLayout(.sizeThatFits)
+    }
+}
